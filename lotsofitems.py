66d2ba37e28c3d29b69faed1cce7f6f10f14d42a
Lewis King <lewisisgood@gmail.com>
2018-07-04 14:03:53 -0700

    Add lots of items

--- lotsofitems.py
+++ lotsofitems.py
@@ -28,10 +28,17 @@ User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
 session.add(User1)
 session.commit()
 
+
 # Items for Soccer
 category1 = Category(user_id=1, name="Soccer")
 
 session.add(category1)
+session.commit()
+
+item1 = Item(user_id=1, name="Soccer ball", description="To kick",
+             category=category1)
+
+session.add(item1)
 session.commit()
 
 item2 = Item(user_id=1, name="Shinguards",
@@ -42,17 +49,16 @@ session.add(item2)
 session.commit()
 
 
-item1 = Item(user_id=1, name="Soccer ball", description="To kick",
-             category=category1)
-
-session.add(item1)
-session.commit()
-
-
 # Items for Tennis
 category2 = Category(user_id=1, name="Tennis")
 
 session.add(category2)
+session.commit()
+
+item1 = Item(user_id=1, name="Tennis ball", description="To hit",
+             category=category2)
+
+session.add(item1)
 session.commit()
 
 item2 = Item(user_id=1, name="Racket", description="To hit with",
@@ -62,17 +68,25 @@ session.add(item2)
 session.commit()
 
 
-item1 = Item(user_id=1, name="Tennis ball", description="To hit",
-             category=category2)
-
-session.add(item1)
-session.commit()
-
 # Items for Baseball
 category3 = Category(user_id=1, name="Baseball")
 
 session.add(category3)
 session.commit()
+
+item1 = Item(user_id=1, name="Base ball", description="To hit with bat",
+             category=category3)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Baseball bat",
+             description="To hit the baseballs",
+             category=category3)
+
+session.add(item2)
+session.commit()
+
 
 # Items for Basketball
 category4 = Category(user_id=1, name="Basketball")
@@ -80,10 +94,38 @@ category4 = Category(user_id=1, name="Basketball")
 session.add(category4)
 session.commit()
 
+item1 = Item(user_id=1, name="Basketball shoes", description="Jordans, etc.",
+             category=category4)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Headbands",
+             description="Keep the sweat out of your eyes",
+             category=category4)
+
+session.add(item2)
+session.commit()
+
+
 # Items for Football
 category5 = Category(user_id=1, name="Football")
 session.add(category5)
 session.commit()
+
+item1 = Item(user_id=1, name="Jersey", description="Rep your team",
+             category=category5)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Helmet",
+             description="Helmets are used to avoid concussions",
+             category=category5)
+
+session.add(item2)
+session.commit()
+
 
 # Items for Squash
 category6 = Category(user_id=1, name="Squash")
@@ -91,10 +133,37 @@ category6 = Category(user_id=1, name="Squash")
 session.add(category6)
 session.commit()
 
+item1 = Item(user_id=1, name="Goggles", description="To protect eyes",
+             category=category6)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Squash ball",
+             description="Small, black, doesn't bounce much",
+             category=category6)
+
+session.add(item2)
+session.commit()
+
+
 # Items for Bocce ball
 category7 = Category(user_id=1, name="Bocce ball")
 
 session.add(category7)
+session.commit()
+
+item1 = Item(user_id=1, name="Mallet", description="To hit with",
+             category=category7)
+
+session.add(item1)
+session.commit()
+
+item2 = Item(user_id=1, name="Horseshoes",
+             description="Hit dem balls through the 'shoes !",
+             category=category7)
+
+session.add(item2)
 session.commit()
 
 
